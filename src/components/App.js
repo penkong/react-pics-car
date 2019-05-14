ae3dc9e45f228e51a49e9318c7b3f49ae52e2da5
mazdak <nazemi.works@gmail.com>
2019-05-14 08:47:37 -0700

    catch input searchbar by parent comp and load in child

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
-import React,{ Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{marginTop:'10px'}}>
-      <SearchBar/>
-    </div>
-  );
+class App extends Component {
+
+  onSearchSubmit (term){
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
+    );
+  }
 }
 
-
-
 export default App;
-ReactDOM.render(<App />,document.querySelector('#root'));
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  state = {}
+  state = { term : ''};
+  
+  onInputChange = (e) => {
+    this.setState({term : e.target.value});
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text"/>
+            <input value={this.state.term} onChange={this.onInputChange} type="text" />
           </div>
         </form>
       </div>
